344dbf12a98eeed67ef1766f13c8576d3429177e
unknown <pedrohcsilva77@gmail.com>
2025-11-27 10:46:53 -0300

    CORS: aceitar apenas front na VM

--- aero_sence_back/src/index.js
+++ aero_sence_back/src/index.js
@@ -5,7 +5,24 @@ import apiRoutes from './routes/index.js';
 
 dotenv.config();
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  'http://172.203.135.173',
+  'http://172.203.135.173:5173'
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error('Not allowed by CORS'));
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use('/api', apiRoutes);
